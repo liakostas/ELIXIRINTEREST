4891af899c03c463f6e38cc60937f2293b4a743a
liakostas <ek4049@stern.nyu.edu>
2024-12-02 18:51:46 -0500

    signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,7 @@
-<script src="../scripts/signup.js" defer></script> 
+const response = await fetch('https://logtodatabase-rgzyvy3rca-uc.a.run.app/signup', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+}); 
