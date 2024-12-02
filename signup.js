a1611429c2908661845abd5ce9757e9ce32a5310
liakostas <ek4049@stern.nyu.edu>
2024-12-02 18:56:16 -0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,7 +1,11 @@
+// Test with POST
 const response = await fetch('https://logtodatabase-rgzyvy3rca-uc.a.run.app/signup', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
     },
     body: JSON.stringify(data)
-}); 
+});
+
+// Or test with GET to see if the endpoint is accessible
+const testResponse = await fetch('https://logtodatabase-rgzyvy3rca-uc.a.run.app/signup'); 
